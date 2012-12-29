adcfd8e30c91b17ff3b65e1c1d5aab4c47fd7ba8
xiekeli <xiekeli@gmail.com>
2012-12-29 11:41:04 +0800

    commit test

---
+++ src/fep/bp/model/UpgradeTaskDAO.java
@@ -0,0 +1,159 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fep.bp.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class UpgradeTaskDAO {
+    private int taskId;
+    private int sequenceCode;
+    private String logicAddress;
+    private int binFileSize;
+    private int binFileID;
+    private InputStream binFile;
+    private Date postTime;
+    private String taskStatus = "0"; //默认未处理
+    private float schedule;//进度
+
+    /**
+     * @return the taskId
+     */
+    public int getTaskId() {
+        return taskId;
+    }
+
+    /**
+     * @param taskId the taskId to set
+     */
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    /**
+     * @return the sequenceCode
+     */
+    public int getSequenceCode() {
+        return sequenceCode;
+    }
+
+    /**
+     * @param sequenceCode the sequenceCode to set
+     */
+    public void setSequenceCode(int sequenceCode) {
+        this.sequenceCode = sequenceCode;
+    }
+
+    /**
+     * @return the logicAddress
+     */
+    public String getLogicAddress() {
+        return logicAddress;
+    }
+
+    /**
+     * @param logicAddress the logicAddress to set
+     */
+    public void setLogicAddress(String logicAddress) {
+        this.logicAddress = logicAddress;
+    }
+
+    /**
+     * @return the binFile
+     */
+    public InputStream getBinFile() {
+        return binFile;
+    }
+    
+    public byte[] getBinFile2ByteArray() throws IOException{
+        byte[] result = new byte[this.binFileSize];
+        binFile.read(result);
+        binFile.close();
+        return result;
+    }
+
+    /**
+     * @param binFile the binFile to set
+     */
+    public void setBinFile(InputStream binFile) {
+        this.binFile = binFile;
+
+    }
+
+    /**
+     * @return the postTime
+     */
+    public Date getPostTime() {
+        return postTime;
+    }
+
+    /**
+     * @param postTime the postTime to set
+     */
+    public void setPostTime(Date postTime) {
+        this.postTime = postTime;
+    }
+
+    /**
+     * @return the taskStatus
+     */
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    /**
+     * @param taskStatus the taskStatus to set
+     */
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    /**
+     * @return the schedule
+     */
+    public float getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * @param schedule the schedule to set
+     */
+    public void setSchedule(float schedule) {
+        this.schedule = schedule;
+    }
+
+    /**
+     * @return the binFileSize
+     */
+    public int getBinFileSize() {
+        return binFileSize;
+    }
+
+    /**
+     * @param binFileSize the binFileSize to set
+     */
+    public void setBinFileSize(int binFileSize) {
+        this.binFileSize = binFileSize;
+    }
+
+    /**
+     * @return the binFileID
+     */
+    public int getBinFileID() {
+        return binFileID;
+    }
+
+    /**
+     * @param binFileID the binFileID to set
+     */
+    public void setBinFileID(int binFileID) {
+        this.binFileID = binFileID;
+    }
+}
---
+++ src/fep/bp/model/UpgradeTaskRowMapper.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fep.bp.model;
+
+import java.io.InputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.lob.OracleLobHandler;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class UpgradeTaskRowMapper implements RowMapper{
+    @Override
+    public Object mapRow(ResultSet rs, int index) throws SQLException{
+        UpgradeTaskDAO task = new UpgradeTaskDAO();
+        task.setTaskId(rs.getInt("TASK_ID"));
+        task.setSequenceCode(rs.getInt("SEQUENCE_CODE"));
+        task.setLogicAddress(rs.getString("LOGICAL_ADDR"));
+        task.setBinFileSize(rs.getInt("BINFILE_SIZE"));
+        task.setBinFile(rs.getBinaryStream("BINFILE"));
+        /*
+        OracleLobHandler lobHandler = new OracleLobHandler();
+        InputStream is =lobHandler.getBlobAsBinaryStream(rs, "BINFILE");
+        task.setBinFile(is);*/
+        
+        task.setTaskStatus(rs.getString("TASK_STATUS"));
+        task.setPostTime(rs.getTimestamp("POST_TIME"));
+        task.setSchedule(rs.getFloat("SCHEDULE"));
+        return task;
+    }
+}
