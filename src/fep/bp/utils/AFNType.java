089969253713a91ce6dda61a3daa1484053e3f20
xiekeli <xiekeli@gmail.com>
2012-12-29 11:41:15 +0800

    commit test

--- src/fep/bp/utils/AFNType.java
+++ src/fep/bp/utils/AFNType.java
@@ -16,5 +16,7 @@ public interface AFNType {
     static byte AFN_GETPARA = 0x0A; //AFN：读取参数
     static byte AFN_TRANSMIT = 0x10; //AFN：数据转发
     static byte AFN_READDATA1 = 0x0C; //AFN：读实时数据
-    static byte AFN_READDATA2 = 0x0D; //AFN：读实时数据
+    static byte AFN_READDATA2 = 0x0D; //AFN：读二类数据
+    static byte AFN_READDATA3 = 0x0E; //AFN：读三类数据
+    static byte AFN_UPGRADE = 0x0F;//AFN：文件传输（远程升级）
 }
